23492c7f69da4ccacb7761290b64055b9250df88
A-545 <potetomaru1128mm@gmail.com>
2025-11-28 01:48:49 +0900

    test.bash実行

--- test.bash
+++ test.bash
@@ -8,18 +8,20 @@ ng () {
 }
 
 res=0
+
+
 ### NORMAL INPUT ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+out=$( 1000 | ./tax)          
+[ "${out}" = "1100" ] || ng "$LINENO"  
+
 ### STRANGE INPUT ###
-out=$(echo あ | ./plus)           #計算できない値を入力してみる
-[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
+out=$(echo あ | ./tax)        
+[ "$?" = 1 ] || ng "$LINENO"  
 [ "${out}" = "" ] || ng "$LINENO"
-out=$(echo | ./plus)              #なにも入力しない
-[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+
+out=$(echo | ./tax)           
+[ "$?" = 1 ] || ng "$LINENO" 
 [ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
-
-
-
