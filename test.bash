b42ec934db986e97c88f1388a479aecdf3eea591
A-545 <potetomaru1128mm@gmail.com>
2025-11-29 20:01:33 +0900

    いろいろ

--- test.bash
+++ test.bash
@@ -27,25 +27,53 @@ out=$(echo 0 | ./tax) #境界値
 out=$(echo 1 | ./tax)
 [ "$out" = 1 ] || ng "$LINENO"
 
+out=$(echo "100 " | ./tax) #末尾空白
+[ "$?" = 0 ] || ng "$LINENO"
+[ "$out" = "110" ] || ng "$LINENO"
+
+out=$(echo " 100" | ./tax) #先頭空白
+[ "$?" = 0 ] || ng "$LINENO"
+[ "$out" = "110" ] || ng "$LINENO"
+
 ### STRANGE INPUT ###
-out=$(echo あ | ./tax) #文字列
-[ "$?" = 1 ] || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
 
-out=$(echo | ./tax) #空行
-[ "$?" = 1 ] || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
+out=$(echo あ | ./tax 2>&1)  # 文字列
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: あ" ] || ng "$LINENO"
 
-out=$(echo 1a | ./tax)
-[ "$?" = 1 ] || ng "$LINENO" #数値に文字が混じる
-[ "$out" = "" ] || ng "$LINENO"
+out=$(echo | ./tax 2>&1)  # 空行
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: " ] || ng "$LINENO"
 
-out=$(printf "100\nabc\n200\n" | ./tax) #複数行に文字列
-[ "$?" = 1 ] || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
+out=$(echo -100 | ./tax 2>&1)  # 負の数
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: -100" ] || ng "$LINENO"
 
-out=$(printf "100\n1a\n200\n" | ./tax) #複数行数値に文字が混じる
-[ "$?" = 1 ] || ng "$LINENO"
-[ "$out" = "" ] || ng "$LINENO"
+out=$(echo 1a | ./tax 2>&1)  # 数字に文字が混じる
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: 1a" ] || ng "$LINENO"
 
-[ "$res" = 0 ] && echo 
+out=$(printf "100\nabc\n200\n" | ./tax 2>&1)  # 複数行に文字列
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: abc" ] || ng "$LINENO"
+
+out=$(printf "100\n1a\n200\n" | ./tax 2>&1)  # 複数行に数字と文字
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: 1a" ] || ng "$LINENO"
+
+out=$(echo 10.0.5 | ./tax 2>&1)  # 小数点が複数
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: 10.0.5" ] || ng "$LINENO"
+
+out=$(printf "100\n\n200\n" | ./tax 2>&1)  # 空行交じり
+status=$?
+[ "$status" = 1 ] || ng "$LINENO"
+[ "$out" = "無効な入力: " ] || ng "$LINENO"
+
